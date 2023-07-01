ea720597c9a7c30037851eda502cee48c5025f14
Tutul-Dark <138271884+Tutul-Dark@users.noreply.github.com>
2023-07-01 12:09:51 +0800

    Update Tutul.py

--- Tutul.py
+++ Tutul.py
@@ -1,4 +1,22 @@
 #Fb link: https://www.facebook.com/profile.php?id=100087428240352&mibextid=ZbWKwL
 # https://github.com/Tutul-Dark/Tutul-Dark
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x05\x00\x00\x>
+import os 
+import requests,bs4,json,os,sys,random,datetime,time,re
+ 
+class Tutul:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.040)
+Tutul('\033[97;1m[\033[92;1m+\033[97;1m] \033[0;92mNEW UPDATE COMING SOON...? ')
+time.sleep(5)
+Tutul('\033[97;1m[\033[92;1m+\033[97;1m] \033[1;93mFOLLOW MY GROUP ')
+os.system(f'xdg-open https://github.com/MR-T4NV1R/')
+time.sleep(7)
+Tutul("\033[97;1m[\033[92;1m+\033[97;1m] \033[0;92mJOIN MY SCRIPT GIFT GROUP")
+time.sleep(5)
+os.system(f'xdg-open https://facebook.com/groups/554714119911648/')
+Tutul("\033[97;1m[\033[92;1m+\033[97;1m] JOIN MESSENGER GROUP")
+time.sleep(5)
+os.system(f'xdg-open https://m.me/j/Aba78QeI-yhUqOwk/')
